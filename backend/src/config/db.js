2449b5011c51c27161306a9f334ae5188079b97b
Mahnoor Tariq <117991270+mahnoor643@users.noreply.github.com>
2026-06-18 22:33:32 +0500

    deployment changes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = mysql.createPool({
+  port: process.env.DB_PORT,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
